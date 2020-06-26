34040ecfb324125747a3d26bb894cb30bad70162
Nobuhiro Ito <iseebi@iseteki.net>
2020-06-26 17:20:50 +0900

    fix Packages.swift

--- Package.swift
+++ Package.swift
@@ -1,16 +1,17 @@
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
-    platforms: [.iOS(.v10)],
     name: "SimpleTwitterAuthentication",
+    platforms: [.iOS(.v10)],
     products: [
         .library(name: "SimpleTwitterAuthentication", targets: ["SimpleTwitterAuthentication"])
     ],
     dependencies: [
-        .package(name: "OAuthSwift", url: "https://github.com/OAuthSwift/OAuthSwift.git", .upToNextMajor(from: "2.1.0"))
+        .package(url: "https://github.com/OAuthSwift/OAuthSwift.git", .upToNextMajor(from: "2.1.0"))
     ],
     targets: [
-        .target(name: "SimpleTwitterAuthentication", path: "Sources")
+        .target(name: "SimpleTwitterAuthentication", dependencies: ["OAuthSwift"], path: "Sources")
     ],
     swiftLanguageVersions: [.v5]
 )
